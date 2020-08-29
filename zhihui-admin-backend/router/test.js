38f7c2bc8c6f701ddf536de635687c3279584d1c
HUIKIM <969791392@qq.com>
2020-08-29 17:35:23 +0800

    update frontend

--- zhihui-admin-backend/router/test.js
+++ zhihui-admin-backend/router/test.js
@@ -7,13 +7,11 @@ const callCloudDB = require('../utils/callCloudDB.js')
 // app.use(router.allowedMethods()) // 允许路由的get post等方法
 
 router.get('/test', async (ctx, next) => {
-  console.log('555')
   const query = `
   db.collection('address').get()
   `
-  const res = await callCloudDB(ctx, 'databasequery', query)
-  console.log(res.data)
-  ctx.body = '恭喜 __小简__ 你成功登陆了'
+  let res = await callCloudDB(ctx, 'databasequery', query)
+  ctx.body = res
 })
 
 module.exports = (app) => {
--- zhihui-admin-backend/utils/callCloudDB.js
+++ zhihui-admin-backend/utils/callCloudDB.js
@@ -2,20 +2,27 @@ const fs = require('fs')
 const getAccessToken = require('./getAccessToken.js')
 const koa2Req = require('koa2-request');
 
-const callCloudDB = async(ctx, fnName = {}) => {
+const callCloudDB = async(ctx, fnName, query = {}) => {
   const ACCESS_TOKEN = await getAccessToken()
   const options = {
     method: 'POST',
     uri: `https://api.weixin.qq.com/tcb/${fnName}?access_token=${ACCESS_TOKEN}`,
     body: {
-        env: ctx.state.env,
+      query,
+      env: ctx.state.env,
     },
     json: true // Automatically stringifies the body to JSON
   }
 
   return await koa2Req(options)
     .then((res) => {
-        return res
+        let errcode = res.body.errcode
+        if(errcode == 0) {
+          return JSON.parse(res.body.data)
+        }
+        else {
+          console.log(res.body.errcode)
+        }
     })
     .catch(function (err) {
         console.log(err);
